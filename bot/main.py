b1f699dbff3cb6c2506442eee5ee83ae651908d6
yun <youngyun147@gmail.com>
2025-02-18 20:00:07 +0600

    Setup entry point

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,16 @@
 import asyncio
 import logging
 import sys
-
+from config import load_config_from_file
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 
 async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    config = load_config_from_file()
+
+    bot = Bot(token=config.telegram_bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
     dp = Dispatcher()
 
